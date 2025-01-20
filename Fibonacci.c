2764d319c8c731b98ada12ca92d4e77afca1049a
Abderrahim Bahraoui <66497970+Abduu898@users.noreply.github.com>
2025-01-20 18:28:29 +0100

    Create Fibonacci.c
    Fibonacci series C code

---
+++ Fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+ int N,i;
+ // les N premiers termes de la séquence de Fibonacci
+ int terme1 = 0, terme2 = 1, termeSuivant;
+ // Demander à l'utilisateur de saisir le nombre de termes
+ printf("Entrez le nombre de termes de la séquence de Fibonacci : ");
+ scanf("%d", &N);
+ printf("Les %d premiers termes de la séquence de Fibonacci sont :\n", N);
+ for ( i = 0; i < N; ++i) {
+ printf("%d ", terme1);
+ termeSuivant = terme1 + terme2;
+ terme1 = terme2;
+ terme2 = termeSuivant;
+ }
+ return 0;
+}
